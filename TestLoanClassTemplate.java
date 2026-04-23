25656148ce45d66b01d902db2aa0fd555281376f
JessicaDPittman <JessicaDPittman1992@icloud.com>
2026-04-23 16:11:14 -0400

    Update TestLoanClassTemplate.java

--- TestLoanClassTemplate.java
+++ TestLoanClassTemplate.java
@@ -56,6 +56,9 @@ public class TestLoanClassTemplate {
 
 	  while (myLoanAmount >= myMonthlyPayment) {
 		  // to do A1 same as A2
+		  interest = myLoanAmount * myMonthlyInterestRate;
+		  
+		  
 		  
 	  }
 	  if (myLoanAmount < myMonthlyPayment) {
